2a37a0749e65e24e6d40ebdb11cc1929c93d705f
Mateusz Banasik <kilifugame@gmail.com>
2024-04-21 01:58:43 +0200

    Add base, probably not fully properly working Path Tracing

--- RayTracer/main.cpp
+++ RayTracer/main.cpp
@@ -13,7 +13,10 @@
 #include <vector>
 #include "PointLight.h"
 #include <algorithm>
+#include <random>
 
+
+#define M_PI 3.14159265358979323846
 
 Vector Reflect(Vector I, Vector N)
 {
@@ -31,29 +34,6 @@ Vector Refract(Vector I, Vector N, const float& ior)
     return r_out_perp + r_out_parallel;
 
 }
-
-
-Vector color(Ray& r, Sphere& sphere, Triangle& triangle, Plane& plane) {
-    r.Direction.Normalize();
-    Vector unitDir = r.Direction;
-    Vector IntersectVector(0.0f, 0.0f, 0.0f);
-    float t = 0.5f * (unitDir.y + 1.0f);
-
-    //if (plane.Intersect(r))
-        //return Vector(0, 0, 1);
-
-    //if (sphere.Intersect(r))
-        //return sphere.colour;
-
-
-    if (triangle.Intersect(r, IntersectVector))
-        return Vector(1.0f, 1.0f, 0.5f);
-
-
-    return Vector(1.0, 1.0, 1.0) * (1.0 - t) + Vector(0.5, 0.7, 1.0f) * t;
-}
-
-
 
 void IntersectObjects(std::shared_ptr<Scene> scene, Ray ray, std::shared_ptr<Object>& closestObject, int& nr, int& nr2) {
     
@@ -78,6 +58,9 @@ void IntersectObjects(std::shared_ptr<Scene> scene, Ray ray, std::shared_ptr<Obj
             }
         }
     }
+
+    if (closestObject == nullptr)
+        return;
 
 
     if (scene->reflectionNumber > 0) {
@@ -112,78 +95,129 @@ void IntersectObjects(std::shared_ptr<Scene> scene, Ray ray, std::shared_ptr<Obj
 
 }
 
+Vector getRandomDirectionHemisphere(Vector& normal) {
+    static std::mt19937 generator(std::random_device{}());
+    static std::uniform_real_distribution<float> distribution(0.0f, 1.0f);
 
-Vector getColour(std::shared_ptr<Scene> scene, float x, float y, int resX, int resY) {
+    // Generowanie losowego kierunku w pó³kuli za pomoc¹ równomiernego rozk³adu punktów na sferze
+    float u1 = distribution(generator);
+    float u2 = distribution(generator);
+
+    float r = sqrt(1.0f - u1 * u1); // promieñ ko³a w pó³kuli
+    float phi = 2 * M_PI * u2; // k¹t fi w zakresie [0, 2*PI]
+
+    // Konwersja wspó³rzêdnych sferycznych na kartezjañskie
+    float x = r * cos(phi);
+    float y = r * sin(phi);
+    float z = u1;
+
+    // Obliczenie kierunku na podstawie normalnej powierzchni
+    Vector tangent, bitangent;
+    if (fabs(normal.x) < fabs(normal.y)) {
+        tangent = Vector(1.0f, 0.0f, 0.0f);
+    }
+    else {
+        tangent = Vector(0.0f, 1.0f, 0.0f);
+    }
+    tangent = (tangent - normal * normal.dotProduct(tangent)).Normalize();
+    bitangent = normal.cross(tangent).Normalize();
+
+    // Wygenerowanie losowego wektora w lokalnym uk³adzie wspó³rzêdnych
+    return (tangent * x + bitangent * y + normal * z).Normalize();
+}
+
+Vector getColour(std::shared_ptr<Scene> scene, Ray ray, int depth) {
+
+    if (depth <= 0) {
+        return Vector(0, 0, 0);
+    }
 
     Vector color = { 0, 0, 0 };
-    Ray ray = scene->camera->GenerateRay(x, y);
 
-    float closestDistance = 1000;
-    bool check = false;
-    bool check2 = false;
     std::shared_ptr<Object> closestObject;
-
-    Vector intersectionPoint;
     int nr = -1;
     int nr2 = -1;
     scene->reflectionNumber = 2;
     IntersectObjects(scene, ray, closestObject, nr, nr2);
 
+    if (closestObject != nullptr) { 
 
-    if (closestObject->GetIntersectionPoint().z != NULL) {
-        color =  color + scene->Light(closestObject->GetIntersectionPoint(), closestObject, ray.Direction.Normalize(), nr);
+        Vector colorMaterial = closestObject->GetMaterial().GetColour();
+        Vector intersectionPoint = closestObject->GetIntersectionPoint();
+        Vector normal = closestObject->GetIntersectionNormal();
+
+        int numRays = 32;
+        Vector indirectLighting = Vector(0, 0, 0);
+
+        for (int i = 0; i < numRays; ++i) {
+            Vector randomDirection = getRandomDirectionHemisphere(normal);
+            Ray indirectRay(intersectionPoint, randomDirection);
+            indirectLighting = indirectLighting + getColour(scene, indirectRay, depth - 1); 
+        }
+
+        indirectLighting = indirectLighting / numRays;
+
+        color = color + scene->Light(closestObject->GetIntersectionPoint(), closestObject, ray.Direction.Normalize(), nr);
+       
+        color.x = color.x * colorMaterial.x;
+        color.y = color.y * colorMaterial.y;
+        color.z = color.z * colorMaterial.z;
+
+        Vector finalColor = (indirectLighting + color);
+
+        return finalColor;
+
     }
     else {
-        color = Vector(0.2, 0.4, 0.85);
+        return Vector(0.2, 0.4, 0.85);
     }
-
-    return color;
 }
 
 Vector Sampling(std::shared_ptr<Scene> scene, int x, int y, int resX, int resY, int maxSteps, float centreX, float centreY, float offset) {
 
+    int depth = 2;
     Vector colour = Vector(0, 0, 0);
     Vector centreColour = Vector(0, 0, 0);
     std::vector<Vector> tempColours;
     tempColours.clear();
 
-    centreColour = getColour(scene, x + centreX, y + centreY, resX, resY);
+    centreColour = getColour(scene, scene->camera->GenerateRay(x + centreX, y + centreY), depth);
     //std::cout << x + centreX << " " << x + (centreX - offset) << " " << x + (centreX + offset) << std::endl;
-    tempColours.push_back(getColour(scene, x + (centreX - offset), y + (centreY - offset), resX, resY));
-    tempColours.push_back(getColour(scene, x + (centreX + offset), y + (centreY - offset), resX, resY));
-    tempColours.push_back(getColour(scene, x + (centreX - offset), y + (centreY + offset), resX, resY));
-    tempColours.push_back(getColour(scene, x + (centreX + offset), y + (centreY + offset), resX, resY));
+    tempColours.push_back(getColour(scene, scene->camera->GenerateRay(x + (centreX - offset), y + (centreY - offset)), depth));
+    tempColours.push_back(getColour(scene, scene->camera->GenerateRay(x + (centreX + offset), y + (centreY - offset)), depth));
+    tempColours.push_back(getColour(scene, scene->camera->GenerateRay(x + (centreX - offset), y + (centreY + offset)), depth));
+    tempColours.push_back(getColour(scene, scene->camera->GenerateRay(x + (centreX + offset), y + (centreY + offset)), depth));
 
-    if (maxSteps > 1) {
-        if ((tempColours[0] - centreColour).GetLength() > 0.4) {
-            //std::cout << "0" << std::endl;
-            tempColours[0] = Sampling(scene, x, y, resX, resY, maxSteps - 1, -offset * 0.5f, -offset * 0.5f, offset * 0.5f);
-        }
-        else {
-            tempColours[0] = (Vector)(tempColours[0] + centreColour) * 0.5;
-        }
-        if ((tempColours[1] - centreColour).GetLength() > 0.4) {
-            //std::cout << "1" << std::endl;
-            tempColours[1] = Sampling(scene, x, y, resX, resY, maxSteps - 1, +offset * 0.5f, -offset * 0.5f, offset * 0.5f);
-        }
-        else {
-            tempColours[1] = (Vector)(tempColours[1] + centreColour) * 0.5;
-        }
-        if ((tempColours[2] - centreColour).GetLength() > 0.4) {
-            //std::cout << "2" << std::endl;
-            tempColours[2] = Sampling(scene, x, y, resX, resY, maxSteps - 1, -offset * 0.5f, +offset * 0.5f, offset * 0.5f);
-        }
-        else {
-            tempColours[2] = (Vector)(tempColours[2] + centreColour) * 0.5;
-        }
-        if ((tempColours[3] - centreColour).GetLength() > 0.4) {
-            //std::cout << "3" << std::endl;
-            tempColours[3] = Sampling(scene, x, y, resX, resY, maxSteps - 1, +offset * 0.5f, +offset * 0.5f, offset * 0.5f);
-        }
-        else {
-            tempColours[3] = (Vector)(tempColours[3] + centreColour) * 0.5;
-        }
-    }
+    //if (maxSteps > 1) {
+    //    if ((tempColours[0] - centreColour).GetLength() > 0.4) {
+    //        //std::cout << "0" << std::endl;
+    //        tempColours[0] = Sampling(scene, x, y, resX, resY, maxSteps - 1, -offset * 0.5f, -offset * 0.5f, offset * 0.5f);
+    //    }
+    //    else {
+    //        tempColours[0] = (Vector)(tempColours[0] + centreColour) * 0.5;
+    //    }
+    //    if ((tempColours[1] - centreColour).GetLength() > 0.4) {
+    //        //std::cout << "1" << std::endl;
+    //        tempColours[1] = Sampling(scene, x, y, resX, resY, maxSteps - 1, +offset * 0.5f, -offset * 0.5f, offset * 0.5f);
+    //    }
+    //    else {
+    //        tempColours[1] = (Vector)(tempColours[1] + centreColour) * 0.5;
+    //    }
+    //    if ((tempColours[2] - centreColour).GetLength() > 0.4) {
+    //        //std::cout << "2" << std::endl;
+    //        tempColours[2] = Sampling(scene, x, y, resX, resY, maxSteps - 1, -offset * 0.5f, +offset * 0.5f, offset * 0.5f);
+    //    }
+    //    else {
+    //        tempColours[2] = (Vector)(tempColours[2] + centreColour) * 0.5;
+    //    }
+    //    if ((tempColours[3] - centreColour).GetLength() > 0.4) {
+    //        //std::cout << "3" << std::endl;
+    //        tempColours[3] = Sampling(scene, x, y, resX, resY, maxSteps - 1, +offset * 0.5f, +offset * 0.5f, offset * 0.5f);
+    //    }
+    //    else {
+    //        tempColours[3] = (Vector)(tempColours[3] + centreColour) * 0.5;
+    //    }
+    //}
 
 
     for (auto c : tempColours) {
@@ -224,7 +258,7 @@ int main(int argv, char** args) {
 
     //make cameras
     std::shared_ptr<OrthographicCamera> orthoCam = std::make_shared<OrthographicCamera>(512, 512, Vector(0,0,5), Vector(0,0,-1), Vector(0,1,0));
-    std::shared_ptr<PerspectiveCamera> perspCam = std::make_shared<PerspectiveCamera>(512,512, 100.0f, 75.0f, Vector(0, 0, 5), Vector(0, 0, -1), Vector(0, 1, 0));
+    std::shared_ptr<PerspectiveCamera> perspCam = std::make_shared<PerspectiveCamera>(256,256, 100.0f, 75.0f, Vector(0, 0, 5), Vector(0, 0, -1), Vector(0, 1, 0));
     
     std::shared_ptr<Camera> camera = perspCam;
 
@@ -233,18 +267,18 @@ int main(int argv, char** args) {
 
     //make objects
     //dont know why but to move sphere up in orthographic camera you have to put -1 intead of 1
-    std::shared_ptr<Sphere> sphere1 = std::make_shared<Sphere>(Vector(-0.5f, -3.0f, -11.5), 1.0f, Material(Vector(0.5f, 0.5f, 0.5f), 128, 5, 2, 0));
+    std::shared_ptr<Sphere> sphere1 = std::make_shared<Sphere>(Vector(-0.5f, -3.0f, -11.5), 1.0f, Material(Vector(0.8f, 0.8f, 0.8f), 128, 5, 0, 0));
     std::shared_ptr<Sphere> sphere2 = std::make_shared<Sphere>(Vector(0, 0.75f, -1), 0.3, Material(Vector(1.0f, 1.0f, 1.0f), 128, 1, 0, 0));
     std::shared_ptr<Sphere> sphere3 = std::make_shared<Sphere>(Vector(1.5f, -3.0f, -12.5), 1.0, Material(Vector(0.1f, 0.5f, 0.5f), 100, 1, 0, 1.05f));
-    std::shared_ptr<Sphere> sphere4 = std::make_shared<Sphere>(Vector(1.5f, -3.0f, -8.5), 1.0f, Material(Vector(0.1f, 0.5f, 0.5f), 128, 20, 0, 1.0f));
+    std::shared_ptr<Sphere> sphere4 = std::make_shared<Sphere>(Vector(1.5f, -3.0f, -8.5), 1.0f, Material(Vector(1.f, 1.f, 1.f), 128, 1, 0, 0.f));
     std::shared_ptr<Plane> plane = std::make_shared<Plane>(Vector(0, -2, 0), Vector(0, 1, 0), Material(Vector(1.f, 1.f, 1.f)));
 
     std::shared_ptr<Plane> P1 = std::make_shared<Plane>(Vector(4, 0, 0), Vector(-1, 0, 0), Material(Vector(0.0f, 0.0f, 1.0f), 128, 1, 0, 0)); //blue
     std::shared_ptr<Plane> P2 = std::make_shared<Plane>(Vector(-4, 0, 0), Vector(1, 0, 0), Material(Vector(1.0f, 0.0f, 0.0f), 128, 1, 0, 0)); //red
-    std::shared_ptr<Plane> P3 = std::make_shared<Plane>(Vector(0, 4, 0), Vector(0, -1, 0), Material(Vector(0.0f, 0.0f, 0.0f), 128, 1, 0, 0)); //black
-    std::shared_ptr<Plane> P4 = std::make_shared<Plane>(Vector(0, -4, 0), Vector(0, 1, 0), Material(Vector(0.5, 0.5f, 0.5f), 128, 1, 0, 0)); //turkusowy
-    std::shared_ptr<Plane> P5 = std::make_shared<Plane>(Vector(0, 0, 16), Vector(0, 0, -1), Material(Vector(0.5f, 0.5f, 0.5f), 128, 1, 0, 0)); //fiolet
-    std::shared_ptr<Plane> P6 = std::make_shared<Plane>(Vector(0, 0, -16), Vector(0, 0, 1), Material(Vector(0.5f, 0.5f, 0.5f), 128, 1, 0, 0)); //zolty
+    std::shared_ptr<Plane> P3 = std::make_shared<Plane>(Vector(0, 4, 0), Vector(0, -1, 0), Material(Vector(0.9f, 0.9f, 0.9f), 128, 1, 0, 0)); //black
+    std::shared_ptr<Plane> P4 = std::make_shared<Plane>(Vector(0, -4, 0), Vector(0, 1, 0), Material(Vector(0.9, 0.9f, 0.9f), 128, 1, 0, 0)); //turkusowy
+    std::shared_ptr<Plane> P5 = std::make_shared<Plane>(Vector(0, 0, 16), Vector(0, 0, -1), Material(Vector(0.9f, 0.9f, 0.9f), 128, 1, 0, 0)); //fiolet
+    std::shared_ptr<Plane> P6 = std::make_shared<Plane>(Vector(0, 0, -16), Vector(0, 0, 1), Material(Vector(0.9f, 0.9f, 0.9f), 128, 1, 0, 0)); //zolty
 
 
     //std::shared_ptr<Sphere> sphere3 = std::make_shared<Sphere>(Vector(-0.5, -0.75, -20), 0.5, Vector(0.5f, 0.2f, 0.1f));
@@ -265,7 +299,7 @@ int main(int argv, char** args) {
 
 
     std::shared_ptr<PointLight> spotLight = std::make_shared<PointLight>(
-        Vector(0, 3, -10),
+        Vector(0, 3, -8),
         LightIntensity(1.0, 1.0, 1.0),
         0.7f,
         0.05f,
